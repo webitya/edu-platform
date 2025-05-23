29719a96cdb11bfcbf05167bbc5dc245caa6be1d
adityawintech <aditya#19AI>
2025-05-23 19:00:49 +0530

    Aditya

--- src/components/global/Navbar.jsx
+++ src/components/global/Navbar.jsx
@@ -59,7 +59,7 @@ const Navbar = () => {
     <header className="w-full shadow-md bg-white z-[9998] transition-all duration-300 py-1">
       <div className="max-w-7xl mx-auto px-4 py-1 flex justify-between items-center">
         <div className="flex items-center gap-2">
-          <img src="/logo.jpg" alt="logo" className="w-18 h-18" />
+          <img src="/logo.jpg" alt="logo" className="w-28 h-28" />
           <span className="text-2xl font-bold text-black">
             <span className="text-yellow-500">L L B</span>
           </span>
